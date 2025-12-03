1fe1e34a2fea99790c2946feda3bbbb48bf34c82
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-03 20:29:55 +0100

    Add print board

---
+++ Battleship with Java/task/src/battleship/Board.java
@@ -0,0 +1,31 @@
+package battleship;
+
+public class Board {
+    private final char[][] grid;
+    private final int size;
+
+    public Board(int size) {
+        this.size = size;
+        this.grid = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = '~';
+            }
+        }
+    }
+
+    public void printBoard() {
+        System.out.print("  ");
+        for (int i = 1; i <= size; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print((char) ('A' + i) + " ");
+            for (int j = 0; j < size; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -3,6 +3,7 @@ package battleship;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Board board = new Board(10);
+        board.printBoard();
     }
 }
